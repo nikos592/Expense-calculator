892c86e887e5a6879796809fdd7bfef8a694c2a8
Ilia Nikulin <wixestrees@gmail.com>
2025-05-31 17:44:10 +0300

    Simplify axios interceptor by removing explicit type annotations

--- calculator/src/services/api.ts
+++ calculator/src/services/api.ts
@@ -13,11 +13,8 @@ const api = axios.create({
 // Добавляем токен к каждому запросу
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) {
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${token}`
-    };
+  if (token && config.headers) {
+    config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
 });
